af37b3e87ec60f9e415ca97bc8707d72294cebd1
Joy Kirii <joykirii@gmail.com>
2021-09-09 20:31:19 +0300

    Create functionality to join and leave voice channels

--- temi_bot.py
+++ temi_bot.py
@@ -1,50 +1,83 @@
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
+from discord import Member
+from discord.ext.commands import has_permissions, MissingPermissions, Bot
 import os
-from discord import member
-from discord.ext.commands import has_permissions, MissingPermissions
 
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix = '!', intents=intents)
 
 load_dotenv()
 
 print(os.getenv('DISCORD_TOKEN'))
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
-
-intents = discord.Intents.default()
-intents.members = True
-
-client = discord.Client(intents=intents)
-
-
 @client.event
 async def on_ready():
+  print('Bot is ready!')
+  print('-------------')
 
-# Functionality
-  general_channel = client.get_channel(878219278376124421)
-
-  await general_channel.send('Bot is online')
+@client.command()
+async def hello(ctx):
+  await ctx.send("Hello i am temi bot")
 
 @client.event
-async def on_message(message):
+async def on_member_join(member):
+  print("Hello + Member")
+
+# client = discord.Client()
+
+# joining and leaving voice channels 
+@client.command(pass_context = True)
+async def join(ctx):
+  if (ctx.author.voice):
+    channel = ctx.message.author.voice.channel
+    await channel.connect()
+  else :
+    await ctx.send("You are not in a voice channel. You must be in a voice channel to run this command.")
+
+@client.command(pass_context = True)
+async def leave(ctx):
+  if (ctx.voice_client):
+    await ctx.guild.voice_client.disconnect()
+    await ctx.send("I have left the voice channel.")
+  else:
+    await ctx.send("I am not in a voice channel.")
+
+
+# client = discord.Client(intents=intents)
+
+
+# @client.event
+# async def on_ready():
+
+# Functionality
+  # general_channel = client.get_channel(878219278376124421)
+
+  # await general_channel.send('Bot is online')
+
+# @client.event
+# async def on_message(message):
   
-  if message.content == 'what is the version':
-    general_channel = client.get_channel(878219278376124421)
+#   if message.content == 'what is the version':
+#     general_channel = client.get_channel(878219278376124421)
 
-    myEmbed = discord.Embed(title='Current Version', description='The bot is in version 1.0', color=0x00ff00,)
-    myEmbed.add_field(name='Version Code',value='v1.0',inline=False)
-    myEmbed.add_field(name='Date Released', value='20/08/21', inline=False)
-    myEmbed.set_footer(text='This is a sample footer')
-    myEmbed.set_author(name='Temi')
+#     myEmbed = discord.Embed(title='Current Version', description='The bot is in version 1.0', color=0x00ff00,)
+#     myEmbed.add_field(name='Version Code',value='v1.0',inline=False)
+#     myEmbed.add_field(name='Date Released', value='20/08/21', inline=False)
+#     myEmbed.set_footer(text='This is a sample footer')
+#     myEmbed.set_author(name='Temi')
 
-    await general_channel.send(embed=myEmbed) 
+#     await general_channel.send(embed=myEmbed) 
 
 # New Member 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
+# class MyClient(discord.Client):
+#     async def on_ready(self):
+#         print(f'Logged in as {self.user} (ID: {self.user.id})')
+#         print('------')
 
     # async def on_member_join(self, member):
     #     guild = member.guild
@@ -62,27 +95,29 @@ async def on_member_join(member):
 
 
 # Kick and Ban Members 
-@client.command()
-@has_permissions(kick_members=True)
-async def kick(ctx, member:discord.Member, *, reason=None):
-  await member.kick(reason=reason)
-  await ctx.send(f'User {member} has been kicked')
+# @client.command()
+# @client.event
+# @has_permissions(kick_members=True)
+# async def kick(ctx, member: discord.Member, *, reason=None):
+#   await member.kick(reason=reason)
+#   await ctx.send(f'User {member} has been kicked')
 
-@kick.error
-async def kick_error(ctx,error):
-  if isinstance(error, commands.MissingPermissions):
-    await ctx.send("You don't have persmission to kick people!")
+# @kick.error
+# async def kick_error(ctx,error):
+#   if isinstance(error, commands.MissingPermissions):
+#     await ctx.send("You don't have persmission to kick people!")
 
-@client.command()
-@has_permissions(ban_members=True)
-async def ban(ctx, member:discord.Member, *, reason=None):
-  await member.ban(reason=reason)
-  await ctx.send(f'User {member} has been banned')
+# @client.command()
+# @client.event
+# @has_permissions(ban_members=True)
+# async def ban(ctx, member:discord.Member, *, reason=None):
+#   await member.ban(reason=reason)
+#   await ctx.send(f'User {member} has been banned')
 
-@ban.error
-async def ban_error(ctx,error):
-  if isinstance(error, commands.MissingPermissions):
-    await ctx.send("You don't have persmission to ban people!")
+# @ban.error
+# async def ban_error(ctx,error):
+#   if isinstance(error, commands.MissingPermissions):
+#     await ctx.send("You don't have persmission to ban people!")
 
 
 
